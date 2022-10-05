ba8e847a76a0aa9d0e531203b8fb4d096e960d58
POmolewa <112206639+POmolewa@users.noreply.github.com>
2022-10-05 11:21:12 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,135 +1,133 @@
-#include "ComplexPlane.h"
-#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
+#include <iostream>
+#include "ComplexPlane.h"
+
+sf::Texture mandelbrot(int width, int height, double xmin, double xmax, double ymin, double ymax, int iterations);
+ComplexPlane Plane;
 
 using namespace sf;
 using namespace std;
-
-void ComplexPlane::Initialize()
+int main()
 {
-	RealX = Xmin;
-	RealWidth = XMax;
-	RealY = Ymin;
-	RealHeight = YMax;
-}
+	bool in = true;
+	int width = VideoMode::getDesktopMode().width;
+	int height = VideoMode::getDesktopMode().height;
 
-int ComplexPlane::getZoom()
-{
-	return MAX_ITER;
-}
+	RenderWindow window(sf::VideoMode(width, height), "Project");
 
+	Texture mandelTexture;
+	Sprite mandelSprite;
 
-void ComplexPlane::ZoomIn(RectangleShape zoomBorder)
-{
-	double x = zoomBorder.getPosition().x - zoomBorder.getSize().x / 2;
-	double y = zoomBorder.getPosition().y - zoomBorder.getSize().y / 2;
+	RectangleShape zoomBorder(Vector2f(width / 2, height / 2));
+	zoomBorder.setFillColor(Color(0, 0, 0, 0));
+	zoomBorder.setOutlineColor(Color(0, 126, 126, 126));
+	zoomBorder.setOutlineThickness(1.0f);
+	zoomBorder.setOrigin(Vector2f(zoomBorder.getSize().x / 2, zoomBorder.getSize().y / 2));
 
-	double x2 = x + zoomBorder.getSize().x;
-	double y2 = y + zoomBorder.getSize().y;
-
-	RealX = MakeReal(x, 0.0, width, Xmin, XMax);
-	RealY = MakeReal(y, 0.0, height, Ymin, YMax);
-
-	RealWidth = MakeReal(x2, 0.0, width, Xmin, XMax);
-	RealHeight = MakeReal(y2, 0.0, height, Ymin, YMax);
-
-	Xmin = RealX;
-	XMax = RealWidth;
-	Ymin = RealY;
-	YMax = RealHeight;
-
-	SetCenterMouseCoords(RealX2, RealY2);
-
-}
-void ComplexPlane::ZoomIn2(RectangleShape zoomBorder2)
-{
-	double x = zoomBorder2.getPosition().x - zoomBorder2.getSize().x / 2;
-	double y = zoomBorder2.getPosition().y - zoomBorder2.getSize().y / 2;
-
-	double x2 = x + zoomBorder2.getSize().x;
-	double y2 = y + zoomBorder2.getSize().y;
-
-	RealX2 = MakeReal(x, 0.0, width, Xmin, XMax);
-	RealY2 = MakeReal(y, 0.0, height, Ymin, YMax);
-}
-
-void ComplexPlane::ZoomOut(RectangleShape zoomBorder2, RectangleShape zoomBorder)
-{
-	double x = zoomBorder.getPosition().x - zoomBorder2.getSize().x / 2;
-	double y = zoomBorder.getPosition().y - zoomBorder2.getSize().y / 2;
-
-	double x2 = x + zoomBorder2.getSize().x;
-	double y2 = y + zoomBorder2.getSize().y;
-
-	RealX = MakeReal(x, 0.0, width, Xmin, XMax);
-	RealY = MakeReal(y, 0.0, height, Ymin, YMax);
-
-	RealWidth = MakeReal(x2, 0.0, width, Xmin, XMax);
-	RealHeight = MakeReal(y2, 0.0, height, Ymin, YMax);
-
-	Xmin = RealX;
-	XMax = RealWidth;
-	Ymin = RealY;
-	YMax = RealHeight;
-
-	SetCenterMouseCoords(RealX2, RealY2);
-}
-
-
-double ComplexPlane::MakeReal(double value, double localMin, double localMax, double min, double max)
-{
-	double Real = (value - localMin) / (localMax - localMin);
-	Real = Real * (max - min);
-	Real += min;
-	return Real;
-}
-
-double ComplexPlane::FindMandle(double cx, double cy)
-{
-	int i = 0;
-	double zr = 0.0, zi = 0.0;
-	while (i < MAX_ITER && zr * zr * zi < 4.0)
-	{
-		double temp = zr * zr - zi * zi + cx;
-		zi = 2.0 * zr * zi + cy;
-		zr = temp;
-		i++;
-	}
-	return i;
-}
-
-void ComplexPlane::SetCenterMouseCoords(double x, double y)
-{
-	MouseX = x;
-	MouseY = y;
-}
-
-string ComplexPlane::LoadText()
-{
-	string txt =
-		"MandelBrot Set \n"
-		"Next Zoom Coords: "  + to_string(RealX2) + " " + to_string(RealY2) + '\n' +
-		"Center: " + to_string(MouseX) + " " + to_string(MouseY) +
-		"\nLeft Click to Zoom in \n"
-		"Right Click to Zoom Out";
-
-	return txt;
+	RectangleShape zoomBorder2(Vector2f(width * 2, height * 2));
+	zoomBorder2.setFillColor(Color(0, 0, 0, 0));
+	zoomBorder2.setOutlineColor(Color(0, 126, 126, 126));
+	zoomBorder2.setOutlineThickness(1.0f);
+	zoomBorder2.setOrigin(Vector2f(zoomBorder2.getSize().x / 2, zoomBorder2.getSize().y / 2));
 	
+	Plane.Initialize();
+	mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
+
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	Text CenterText;
+	CenterText.setFont(font);
+	CenterText.setFillColor(sf::Color::White);
+	CenterText.setCharacterSize(24);
+	
+
+	while (window.isOpen())
+	{
+		Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			
+			if (evnt.type == Event::Closed)
+            {
+                window.close();
+			}
+		}
+		
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			Plane.ZoomIn(zoomBorder);
+			mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
+		}
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+		{
+			in = false;
+			Plane.ZoomOut(zoomBorder2, zoomBorder);
+			mandelTexture = mandelbrot(width, height, Plane.get_Rx(), Plane.get_Rw(), Plane.get_Ry(), Plane.get_Rh(), Plane.getZoom());
+			
+		}
+
+		Plane.ZoomIn2(zoomBorder);
+		CenterText.setPosition(Vector2f(0, 32));
+		CenterText.setString(Plane.LoadText());
+		
+		zoomBorder.setPosition(Mouse::getPosition(window).x, Mouse::getPosition(window).y);
+
+		mandelSprite.setTexture(mandelTexture);
+
+		window.clear(sf::Color::White);
+		window.draw(mandelSprite);
+		window.draw(CenterText);
+
+
+		if (in == true)
+		{
+			window.draw(zoomBorder);
+		}
+		else
+		{
+			window.draw(zoomBorder2);
+		}
+
+		window.display();
+	}
+
+	return 0;
 }
 
-double ComplexPlane::get_Rx()
+Texture mandelbrot(int width, int height, double xmin, double xmax, double ymin, double ymax, int iterations)
 {
-	return RealX;
-}
-double ComplexPlane::get_Ry()
-{
-	return RealY;
-}
-double ComplexPlane::get_Rw()
-{
-	return RealWidth;
-}
-double ComplexPlane::get_Rh()
-{
-	return RealHeight;
+	Texture texture;
+	texture.create(width, height);
+
+	Uint8* pixels = new Uint8[width * height * 4];
+
+	for (int index = 0; index < width; index++)
+	{
+		for (int i = 0; i < height; i++)
+		{
+			double x = xmin + (xmax - xmin) * index / (width - 1.0);
+			double y = ymin + (ymax - ymin) * i / (height - 1.0);
+
+			int Ipoint = Plane.FindMandle(x, y);
+
+			int ppos = 4 * (width * i + index);
+
+			int hue = ((Ipoint * Ipoint) % 256);
+			int sat = ((sqrt(static_cast<int>(Ipoint) % 256)));
+			int val = ((Ipoint * Ipoint) % 256);
+
+			Color IterationstoRGB = Color (hue, sat, val);
+			pixels[ppos] = (int)IterationstoRGB.r;
+			pixels[ppos + 1] = (int)IterationstoRGB.g;
+			pixels[ppos + 2] = (int)IterationstoRGB.b;
+			pixels[ppos + 3] = 255;
+		}
+	}
+
+	texture.update(pixels, width, height, 0, 0);
+
+	delete[] pixels;
+
+	return texture;
 }
